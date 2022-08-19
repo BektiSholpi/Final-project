904a2f22be6cbcce78d74336f6e67ac99e4951d6
BektiSholpi <Bektisolpan@gmail.com>
2022-08-19 15:26:56 +0200

    First commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="containerIn">
+    <div class="headingIn">
+      <h1>Log in to Task App</h1>
+      <h2>Start Organizing your tasks today!</h2>
+
+      <form @submit.prevent="signIn">
+        <label for="email">Email</label>
+        <input
+          type="text"
+          v-model="email"
+          placeholder="email@gmail.com"
+          required
+        />
+        <label for="password">Password</label>
+        <input
+          type="password"
+          v-model="password"
+          placeholder="*********"
+          required
+        />
+        <br />
+        <input type="submit" required />
+      </form>
+      <div>Start Organizing your tasks todays!</div>
+      <div><PersonalRouter :route="route" :buttonText="buttonText" /></div>
+    </div>
+    <div class="img">
+      <img
+        src="https://www.impressline.com.mx/public/uploads/productos/ESC-543t.jpg"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+import PersonalRouter from "./PersonalRouter.vue";
+import { supabase } from "../supabase";
+import { useRouter } from "vue-router";
+import { useUserStore } from "../stores/user";
+import { storeToRefs } from "pinia";
+
+// Route Variables
+const route = "/auth/sign-up";
+const buttonText = "Sign Up!";
+
+// Input Fields
+const email = ref("");
+const password = ref("");
+
+// Error Message
+const errorMsg = ref("");
+
+//Show hide password variables
+const passwordFieldType = computed(() =>
+  hidePassword.value ? "password" : "text"
+);
+const hidePassword = ref(true);
+
+// Router to push user once SignedIn to the HomeView
+const redirect = useRouter();
+
+// Arrow function to Signin user to supaBase
+const signIn = async () => {
+  try {
+    // calls the user store and send the users info to backend to logIn
+    await useUserStore().signIn(email.value, password.value);
+    // redirects user to the homeView
+    redirect.push({ path: "/" });
+  } catch (error) {
+    // displays error message
+    errorMsg.value = `Error: ${error.message}`;
+    // hides error message
+    setTimeout(() => {
+      errorMsg.value = null;
+    }, 5000);
+  }
+};
+</script>
+
+<style>
+.wu-text {
+  color: black;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  margin: 1rem 0;
+}
+.input {
+  color: black;
+  margin-bottom: 1rem;
+}
+.button {
+  background-color: #4caf50; /* Green */
+  border: none;
+  color: white;
+  padding: 10px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+.containerIn {
+  display: flex;
+  flex-direction: row;
+}
+.headingIn {
+  margin-top: 100px;
+}
+</style>
